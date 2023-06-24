652b591db659eb03d4854c0d2e37da182eb37a0f
Kibon <gabriel120026@hotmail.com>
2023-06-23 22:29:16 -0300

    Add arduino, photos, facetest and notebook

--- servo_wireless/servo_wireless.ino
+++ servo_wireless/servo_wireless.ino
@@ -1,20 +1,12 @@
-#include <nRF24L01.h>
-#include <RF24.h>
-#include <RF24_config.h>
 #include <Servo.h>
 
-RF24 radio(7, 8);
-byte endereco[6] = "1node";
-boolean abrirPorta;
 Servo myservo;
+
 void setup() {
   myservo.attach(9);
-  radio.openReadingPipe(1, endereco);
-  radio.startListening();
 }
 
-void ServoClose()
-{
+void ServoClose(){
   for (int pos = 270; pos >= 20; pos -= 10) { 
     myservo.write(pos);
     delay(10);
@@ -30,13 +22,9 @@ void ServoOpen()
 }
 
 void loop() {
-  
-  if (radio.available()){
-    radio.read(&abrirPorta, sizeof(boolean));
-    if (abrirPorta) {
-      ServoOpen();
-      delay(10000);
-      ServoClose();
-    }
-  }
+    ServoOpen();
+    delay(10000);
+    ServoClose();
+    delay(10000);
+     
 }
